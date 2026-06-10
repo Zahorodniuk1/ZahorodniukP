e87468ebd6dcc2beb959bd7818cb30859a882f8d
Zahorodniuk1 <i.zahodnyuk@e-u.edu.ua>
2026-06-10 12:37:56 +0300

    Practical 3 task04 grading students

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,1 +1,24 @@
 // https://www.hackerrank.com/challenges/grading/problem
+pub fn grading_students(grades: &[i32]) -> Vec<i32> {
+    grades.iter().map(|&g| {
+        if g < 38 {
+            return g;
+        }
+        let next_multiple = ((g / 5) + 1) * 5;
+        if next_multiple - g < 3 {
+            next_multiple
+        } else {
+            g
+        }
+    }).collect()
+}
+
+#[cfg(test)]
+mod task04 {
+    use super::*;
+
+    #[test]
+    fn test0() {
+        assert_eq!(grading_students(&[73, 67, 38, 33]), vec![75, 67, 40, 33]);
+    }
+}

be2708e4a44c384b4b52d193436f579f7563703e
Zahorodniuk1 <i.zahodnyuk@e-u.edu.ua>
2026-06-10 13:56:27 +0300

    Practical 10 task10 sales by match

--- src/hackerrank/task09.rs
+++ src/hackerrank/task09.rs
@@ -1,1 +1,21 @@
 // https://www.hackerrank.com/challenges/migratory-birds/problem
+
+pub fn migratory_birds(arr: &[i32]) -> i32 {
+    let mut counts = [0i32; 6];
+    for &b in arr {
+        counts[b as usize] += 1;
+    }
+    let max = *counts[1..=5].iter().max().unwrap();
+    (1..=5).find(|&i| counts[i] == max).unwrap() as i32
+}
+
+#[cfg(test)]
+mod task09 {
+    use super::*;
+
+    #[test]
+    fn test0() {
+        assert_eq!(migratory_birds(&[1, 4, 4, 4, 5, 3]), 4);
+        assert_eq!(migratory_birds(&[1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]), 3);
+    }
+}
--- src/hackerrank/task10.rs
+++ src/hackerrank/task10.rs
@@ -1,1 +1,19 @@
 // https://www.hackerrank.com/challenges/sock-merchant/problem
+
+pub fn sock_merchant(ar: &[i32]) -> i32 {
+    let mut counts = std::collections::HashMap::new();
+    for &s in ar {
+        *counts.entry(s).or_insert(0) += 1;
+    }
+    counts.values().map(|&c| c / 2).sum()
+}
+
+#[cfg(test)]
+mod task10 {
+    use super::*;
+
+    #[test]
+    fn test0() {
+        assert_eq!(sock_merchant(&[10, 20, 20, 10, 10, 30, 50, 10, 20]), 3);
+    }
+}

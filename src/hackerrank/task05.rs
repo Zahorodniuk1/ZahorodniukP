b8d03fc5c58cd4aac90dfa7e2035b9d71b7b00ca
Zahorodniuk1 <i.zahodnyuk@e-u.edu.ua>
2026-06-10 12:52:16 +0300

    Practical 5 task06 kangaroo

--- src/hackerrank/task05.rs
+++ src/hackerrank/task05.rs
@@ -1,1 +1,18 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem
+
+pub fn count_apples_and_oranges(s: i32, t: i32, a: i32, b: i32, apples: &[i32], oranges: &[i32]) {
+    let apple_count = apples.iter().filter(|&&d| a + d >= s && a + d <= t).count();
+    let orange_count = oranges.iter().filter(|&&d| b + d >= s && b + d <= t).count();
+    println!("{}", apple_count);
+    println!("{}", orange_count);
+}
+
+#[cfg(test)]
+mod task05 {
+    use super::*;
+
+    #[test]
+    fn test0() {
+        count_apples_and_oranges(7, 11, 5, 15, &[-2, 2, 1], &[5, -6]);
+    }
+}
--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,19 @@
 // https://www.hackerrank.com/challenges/kangaroo/problem
+pub fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> &'static str {
+    if v1 > v2 && (x2 - x1) % (v1 - v2) == 0 {
+        "YES"
+    } else {
+        "NO"
+    }
+}
+
+#[cfg(test)]
+mod task06 {
+    use super::*;
+
+    #[test]
+    fn test0() {
+        assert_eq!(kangaroo(0, 3, 4, 2), "YES");
+        assert_eq!(kangaroo(0, 2, 5, 3), "NO");
+    }
+}

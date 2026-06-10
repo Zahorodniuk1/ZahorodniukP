cab4300bca98a7199128d39e88e3885aace93d1d
Zahorodniuk1 <i.zahodnyuk@e-u.edu.ua>
2026-06-10 12:09:00 +0300

    Practical 2 task03 staircase results

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,19 @@
 // https://www.hackerrank.com/challenges/staircase/problem
+
+pub fn staircase(n: i32) {
+    for i in 1..=n {
+        let spaces = " ".repeat((n - i) as usize);
+        let hashes = "#".repeat(i as usize);
+        println!("{}{}", spaces, hashes);
+    }
+}
+
+#[cfg(test)]
+mod task03 {
+    use super::*;
+
+    #[test]
+    fn test0() {
+        staircase(6);
+    }
+}
